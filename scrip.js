176b388619c8e66fe827df27cef473cc25128355
unknown <jonathan.garcia130207@gmail.com>
2024-10-19 13:16:55 -0400

    jg2

--- scrip.js
+++ scrip.js
@@ -1,11 +1,12 @@
-console.log ("Hola mundo");
-    for(let cont = 0; cont <= 20; cont++){
-        if (cont % 2 == 0){
-          console.log(`${cont} es par `);
-        } else{
-          console.log(`${cont} es impar `);
+let bton = document.getElementById("jg");
 
+bton.addEventListener('click', (e) => {
+    for(let cont = 0; cont <= 20; cont++){
+        cont = cont;
+        if (cont%2 == 0){
+            console.log(`${cont} es par `);
+        } else{
+           console.log(`${cont} es impar `);
         }
     }
-
-
+});
