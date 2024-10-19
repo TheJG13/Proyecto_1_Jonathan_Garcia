41f28622ac441c76dcecaa1ffe9452d945d20e68
unknown <jonathan.garcia130207@gmail.com>
2024-10-19 13:02:57 -0400

    Prueba

--- scrip.js
+++ scrip.js
@@ -1,1 +1,11 @@
 console.log ("Hola mundo");
+    for(let cont = 0; cont <= 20; cont++){
+        if (cont % 2 == 0){
+          console.log(`${cont} es par `);
+        } else{
+          console.log(`${cont} es impar `);
+
+        }
+    }
+
+
